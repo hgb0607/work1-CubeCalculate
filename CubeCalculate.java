764dac7c3c10273bfb8e7f43053f4c8b9fa574ef
hgb0607 <hgb0607@outlook.com>
2019-03-17 20:07:27 +0800

    优化运算速度

--- CubeCalculate.java
+++ CubeCalculate.java
@@ -1,4 +1,4 @@
-/*** 这是一个计算判断连续正整数立方和是否是另一个正整数立方的小程序
+﻿/*** 这是一个计算判断连续正整数立方和是否是另一个正整数立方的小程序
 * @author hgb0607
 * @version 1.0
 ***/
@@ -15,9 +15,14 @@ public class CubeCalculate {
 		Scanner in = new Scanner(System.in);
 		
 		/*选择模式**/
-		System.out.println("选择需要运行的模式：\n1.手动输入\n2.自动计算不大于1000内的连续正整数");
-		int mode = in.nextInt();
+		int mode;
+		do {
+			System.out.println("选择需要运行的模式：\n1.手动输入\n2.自动计算不大于1000内的连续正整数");
+			mode = in.nextInt();
+		} while ((mode!=1)&&(mode!=2));
 		
+		
+		/*模式一：手动输入**/
 		if (mode==1) {
 			
 		System.out.println("输入连续正整数的第一个数字：");		/*获得第一项的数值**/
@@ -33,19 +38,21 @@ public class CubeCalculate {
 			System.out.println("输入连续正整数的最后一个数字(必须大于之前的数值"+a+")：");
 			b = in.nextInt();		
 		}
-
-		double sum = sum(a,b);							/*调用函数计算并判断**/
+		
+		
+		
+		int sum = sum(a,b);							/*调用函数计算并判断**/
 		int flag = CubeOrNot(sum);
 		Output(a,b,flag,mode);								/*输出结果**/
 		}
 		
 		/*模式二：自动计算**/
 		else if (mode==2) {
-			for (int x = 1; x <= 1000; x++) {				/*穷举相加项数，2项起至100项**/
+			for (int x = 1; x <= 1000; x++) {				/*穷举相加项数，2项起至1000项**/
 				
 				for (int y = 1; y <= 1000-x; y++) {		/*连续正整数第一项从1开始穷举**/
 					
-					double sum = sum(y,y+x);				/*调用函数计算并判断**/
+					int sum = sum(y,y+x);				/*调用函数计算并判断**/
 					int flag = CubeOrNot(sum);
 					Output(y,y+x,flag,mode);					/*输出结果**/
 					
@@ -55,11 +62,8 @@ public class CubeCalculate {
 
 		}
 		
-		/*错误情况**/
-		else {
-			System.out.println("输入有误！！！");
-		}
-		
+
+		in.close();
 	}
 		
 
@@ -70,9 +74,9 @@ public class CubeCalculate {
 	 * @param num2 连续正整数中的最大项
 	 * @return 输出立方和
 	 **/
-	private static double sum(double num1,double num2){
-		double SumTemp = 0;
-		int k = (int) (num2-num1);
+	private static int sum(int num1,int num2){
+		int SumTemp = 0;
+		int k = num2-num1;
 		for (int i = 0; i <= k; i++) {
 			SumTemp = SumTemp+num1*num1*num1;
 			num1++;
@@ -84,10 +88,10 @@ public class CubeCalculate {
 	 * @param num 立方和
 	 * @return 输出立方根或非正整数根状态值
 	 **/
-	private static int CubeOrNot(double num) {
+	private static int CubeOrNot(int num) {
 		double cbrt = Math.cbrt(num);
 		if ( cbrt%1 == 0 ) {
-			return (int) cbrt;
+			return (int)cbrt;
 		}
 		return 0;
 	}
@@ -99,15 +103,14 @@ public class CubeCalculate {
 	 * @param mode 选择模式
 	 * @return 输出显示结果到屏幕
 	 **/
-	public static void Output(double min,double max ,int cbrt,int mode) {		
+	public static void Output(int min,int max ,int cbrt,int mode) {		
 		if (cbrt==0) {
 			if(mode==1)
-			System.out.println("从"+ (long)min + "到" + (long)max + "的连续正整数的立方和不是任何正整数的立方");
+			System.out.println("从"+ min + "到" + max + "的连续正整数的立方和不是任何正整数的立方");
 			else {}
 		} else {
-			System.out.println("从"+ (long)min + "到" + (long)max + "的连续正整数的立方和为" + cbrt + "的立方");
+			System.out.println("从"+ min + "到" + max + "的连续正整数的立方和为" + cbrt + "的立方");
 		}
 	}
 
-	
 }
